c91ab1fdd4395cbcb96a0e179463a88e8cffaf84
Jeremie Desgagne-Bouchard <jerdesga@IAD.CA.INET>
2017-08-06 15:07:34 -0400

    rnn cudnn

--- blog/rnn.unroll.R
+++ blog/rnn.unroll.R
@@ -191,3 +191,115 @@ rnn.unroll.cudnn <- function(num.rnn.layer,
   } else return(loss)
 }
 
+
+# single shot rnn
+rnn.unroll.cudnn <- function(num.rnn.layer, 
+                             seq.len, 
+                             input.size,
+                             num.embed, 
+                             num.hidden,
+                             num.label,
+                             dropout=0,
+                             ignore_label=0,
+                             init.state=NULL,
+                             config,
+                             cell.type="lstm",
+                             output_last_state=F) {
+  
+  embed.weight <- mx.symbol.Variable("embed.weight")
+  rnn.weight <- mx.symbol.Variable("rnn.weight")
+  rnn.state.weight <- mx.symbol.Variable("rnn.state.weight")
+  cls.weight <- mx.symbol.Variable("cls.weight")
+  cls.bias <- mx.symbol.Variable("cls.bias")
+  
+  # embeding layer
+  label <- mx.symbol.Variable("label")
+  data <- mx.symbol.Variable("data")
+  # data_mask_array <- mx.symbol.Variable("data.mask.array")
+  # data_mask_array <- mx.symbol.stop_gradient(data_mask_array, name = "data.mask.array")
+  
+  data <- mx.symbol.transpose(data=data)
+  embed <- mx.symbol.Embedding(data=data, input_dim=input.size,
+                               weight=embed.weight, output_dim=num.embed, name="embed")
+  
+  # wordvec <- mx.symbol.split(data=embed, axis=1, num.outputs=seq.len, squeeze_axis=F)
+  # data_mask_split <- mx.symbol.split(data=data_mask_array, axis=1, num.outputs=seq.len, squeeze_axis=F)
+  
+  last.hidden <- list()
+  last.states <- list()
+  decode <- list()
+  softmax <- list()
+  fc <- list()
+  
+  rnn <- mx.symbol.RNN(data=embed, state=rnn.state.weight, parameters=rnn.weight, state.size=num.hidden, num.layers=num.rnn.layer, bidirectional=F, mode=cell.type, state.outputs=F, p=dropout, name=paste(cell.type, num.rnn.layer, "layer", sep="_"))
+  
+  # Decoding
+  # if (config=="one-to-one") {
+  #   last.hidden <- c(last.hidden, next.state[[1]])
+  # }
+  
+  if (config=="seq-to-one") {
+    last.seq <- mx.symbol.SequenceLast(rnn[[1]], name = "last.seq")
+    fc <- mx.symbol.FullyConnected(data=last.seq,
+                                   weight=cls.weight,
+                                   bias=cls.bias,
+                                   num.hidden=num.label)
+    
+    loss <- mx.symbol.SoftmaxOutput(data=fc, name="sm", label=label, ignore_label=ignore_label)
+    
+  } else if (config=="one-to-one"){
+    
+    last.hidden_expand = lapply(last.hidden, function(i) mx.symbol.expand_dims(i, axis=1))
+    concat <-mx.symbol.concat(last.hidden_expand, num.args = seq.len, dim = 1)
+    reshape = mx.symbol.reshape(concat, shape=c(num.hidden, -1))
+    
+    fc <- mx.symbol.FullyConnected(data=reshape,
+                                   weight=cls.weight,
+                                   bias=cls.bias,
+                                   num.hidden=num.label)
+    
+    label <- mx.symbol.reshape(data=label, shape=c(-1))
+    loss <- mx.symbol.SoftmaxOutput(data=fc, name="sm", label=label, ignore_label=ignore_label)
+    
+  }
+  
+  if (output_last_state){
+    group <- mx.symbol.Group(c(unlist(last.states), loss))
+    return(group)
+  } else return(loss)
+}
+
+
+# RNN test
+# data <- mx.symbol.Variable("data")
+# embed.weight <- mx.symbol.Variable("embed.weight")
+# rnn.state.weight <- mx.symbol.Variable("rnn.state.weight")
+# rnn.params.weight <- mx.symbol.Variable("rnn.params.weight")
+# 
+# batch.size <- 32
+# seq.len <- 5
+# input.size <- 50
+# num.embed <- 8
+# num.hidden <- 10
+# seqidx <- 1
+# num.rnn.layer <- 2
+# 
+# data <- mx.symbol.transpose(data=data)
+# embed <- mx.symbol.Embedding(data=data, input_dim=input.size, weight=embed.weight, output_dim=num.embed, name="embed")
+# rnn <- mx.symbol.RNN(data=embed, state=rnn.state.weight, parameters=rnn.params.weight, state.size=num.hidden, num.layers=num.rnn.layer, bidirectional=F, mode=cell.type, state.outputs=T, p=dropout, name=paste(cell.type, num.rnn.layer, "layer", seqidx, sep="_"))
+# last.state <- mx.symbol.SequenceLast(rnn[[1]])
+# 
+# rnn$infer.shape(list(data=c(5, 32)))
+# rnn$infer.shape(list(data=c(32, 5)))
+# 
+# last.state$infer.shape(list(data=c(5, 32)))
+# last.state$infer.shape(list(data=c(32, 5)))
+# 
+# embed$infer.shape(list(data=c(5, 32)))
+# embed$infer.shape(list(data=c(32, 5)))
+# 
+# embed <- mx.symbol.Embedding(data=data, input_dim=input.size, weight=embed.weight, output_dim=num.embed, name="embed")
+# wordvec <- mx.symbol.split(data=embed, axis=1, num.outputs=seq.len, squeeze_axis=F)
+# rnn <- mx.symbol.RNN(data=wordvec[[1]], state=rnn.state.weight, parameters=rnn.params.weight, state.size=num.hidden, num.layers=num.rnn.layer, bidirectional=F, mode=cell.type, state.outputs=T, p=dropout, name=paste(cell.type, num.rnn.layer, "layer", seqidx, sep="_"))
+# last.state <- mx.symbol.SequenceLast(rnn[[1]])
+# rnn$infer.shape(list(data=c(5, 32)))
